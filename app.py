50f9f2867c96cb0b305a10b0b165f5bb52f6e186
Garima Agarwal <154506037+agarwalgarimap@users.noreply.github.com>
2025-01-21 21:13:02 +0530

    app.py

--- app.py
+++ app.py
@@ -5,24 +5,19 @@ from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
 
-
-# Streamlit app
+## Streamlit App
 st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
 st.title("🦜 LangChain: Summarize Text From YT or Website")
 st.subheader('Summarize URL')
 
-# Get Groq API Key and URL
+## Get the Groq API Key and URL (YT or website) to be summarized
 with st.sidebar:
     groq_api_key = st.text_input("Groq API Key", value="", type="password")
 
 generic_url = st.text_input("URL", label_visibility="collapsed")
 
-# Gemma Model using Groq API
-try:
-    llm = ChatGroq(model="Gemma-7b-It", groq_api_key=groq_api_key)
-except Exception as e:
-    st.error(f"Error initializing ChatGroq model: {e}")
-    st.stop()
+## Gemma Model Using Groq API
+llm = ChatGroq(model="Gemma-7b-It", groq_api_key=groq_api_key)
 
 prompt_template = """
 Provide a summary of the following content in 300 words:
@@ -31,32 +26,34 @@ Content:{text}
 prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
 
 if st.button("Summarize the Content from YT or Website"):
-    # Validate inputs
+    ## Validate all the inputs
     if not groq_api_key.strip() or not generic_url.strip():
         st.error("Please provide the information to get started")
     elif not validators.url(generic_url):
         st.error("Please enter a valid URL. It can be a YT video URL or website URL")
-
     else:
         try:
             with st.spinner("Waiting..."):
-                # Loading the website or YouTube video data
+                ## loading the website or YT video data
                 if "youtube.com" in generic_url:
-                    loader = YoutubeLoader.from_youtube_url(generic_url, add_video_info=True)
+                    try:
+                        loader = YoutubeLoader.from_youtube_url(generic_url, add_video_info=True)
+                        docs = loader.load()
+                    except Exception as e:
+                        st.error(f"An error occurred while accessing the YouTube video: {e}")
+                        return  # Exit the function if there's an error
                 else:
-                    loader = UnstructuredURLLoader(
-                        urls=[generic_url],
-                        ssl_verify=False,
-                        headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
-                    )
-                docs = loader.load()
+                    loader = UnstructuredURLLoader(urls=[generic_url], ssl_verify=False,
+                                                    headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
+                    docs = loader.load()
 
-                # Chain for summarization
+                ## Chain For Summarization
                 chain = load_summarize_chain(llm, chain_type="stuff", prompt=prompt)
                 output_summary = chain.run(docs)
 
                 st.success(output_summary)
         except Exception as e:
-            st.error(f"An error occurred while summarizing the content: {e}")
+            st.exception(f"Exception: {e}")
+
             st.exception(e)
 
